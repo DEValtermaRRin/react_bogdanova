ad8049c6bc55ae4dcfc22c3cbc6eeebb9b88e458
altermaRRin <dev.altermarrin@gmail.com>
2022-04-13 10:06:42 +0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Form } from './components/func/Form';
 import { MessageList } from './components/func/MessageList';
--- src/components/func/MessageList.jsx
+++ src/components/func/MessageList.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 export const MessageList = ({ messages }) => (
   <div className="msg">
     {messages.map((msg, idx) => (
